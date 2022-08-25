7f0ccc0f3c59d1115c89b4c0c830c11660128785
SevenOmens <ajpbmusic@gmail.com>
2022-08-25 10:51:09 +0100

    delete comment created, comments sorted by most recent first by default

--- src/Components/ArticleComments.jsx
+++ src/Components/ArticleComments.jsx
@@ -1,15 +1,20 @@
 import { getArticleComments } from "../Api";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import AddComment from "./AddComment";
+import { UserContext } from "../Contexts/LoggedInUser";
+import DeleteComment from "./DeleteComment";
 
 export default function ArticleComments({ article_id }) {
   const [commentData, setCommentData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  console.log(commentData);
+  const { value } = useContext(UserContext);
 
   useEffect(() => {
     getArticleComments(article_id).then((data) => {
-      setCommentData(data);
+      const sortedComments = data.sort((a, b) =>
+        a.created_at < b.created_at ? 1 : -1
+      );
+      setCommentData(sortedComments);
       setIsLoading(false);
     });
   }, [article_id]);
@@ -24,6 +29,13 @@ export default function ArticleComments({ article_id }) {
             <div>{body}</div>
             <div>{author}</div>
             <div>Votes:{votes}</div>
+            {value.username === author ? (
+              <DeleteComment
+                comment_id={comment_id}
+                commentData={commentData}
+                setCommentData={setCommentData}
+              />
+            ) : null}
           </div>
         );
       })}
